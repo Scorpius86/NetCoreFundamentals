721ec2e9bfbbbf5e90fa306f063cda390582d59c
scorpius86 <scorpius86@hotmail.com>
2020-02-23 13:30:40 -0500

    Cambios

---
+++ 01 - Net Core Fundamentals/01/NetCore.Console/NetCore.Console.Client/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NetCore.Console.Client
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Visual Studio 2019");
+        }
+    }
+}
