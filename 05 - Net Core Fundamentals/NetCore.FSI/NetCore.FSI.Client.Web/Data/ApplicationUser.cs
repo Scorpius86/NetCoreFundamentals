3360d34c4c79ceec894c4d9586c9892ad756cc3b
Erick Arostegui Cunza <scorpius86@gmail.com>
2020-04-05 14:33:21 -0500

    Asp.Net Core Identity

---
+++ 05 - Net Core Fundamentals/NetCore.FSI/NetCore.FSI.Client.Web/Data/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore.FSI.Client.Web.Data
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [PersonalData]
+        public DateTime CareerSatarted { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ 05 - Net Core Fundamentals/NetCore.FSI/NetCore.FSI.Client.Web/Data/Migrations/20200405192230_User-CareerStarted.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NetCore.FSI.Client.Web.Data.Migrations
+{
+    public partial class UserCareerStarted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CareerSatarted",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CareerSatarted",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetUsers");
+        }
+    }
+}
